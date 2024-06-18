b69798306bb9d37f8ddc9a7c9dd22d580cd963bd
Paul Greetham <pgreetham@protonmail.com>
2024-06-18 16:09:34 +0200

    sizing changed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,10 +5,15 @@ import './Header.scss';
 const Header: React.FC = () => {
   return (
     <header>
-      <h1>Found and Ground</h1>
+      <h1 className="site-title">
+        <span className="name">
+          <span className="column column-1">F<span className="rest">ound</span></span>
+          <span className="column column-2"><span className="initial">&</span></span>
+          <span className="column column-3">G<span className="rest">round</span></span>
+        </span>
+      </h1>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
           <li><Link to="/blog">Blog</Link></li>
           <li><Link to="/about">About</Link></li>
         </ul>

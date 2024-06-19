b93514daa805db2f758f8f2873159d014941519d
Paul Greetham <pgreetham@protonmail.com>
2024-06-19 15:29:04 +0200

    h1 design moved forward

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,18 +7,22 @@ const Header: React.FC = () => {
     <header>
       <h1 className="site-title">
         <span className="name">
-          <span className="initial">F</span>
-          <span className="rest">O</span>
-          <span className="rest">U</span>
-          <span className="rest">N</span>
-          <span className="rest">D</span>
-          <span className="rest">+</span>
-          <span className="initial">G</span>
-          <span className="rest">R</span>
-          <span className="rest">O</span>
-          <span className="rest">U</span>
-          <span className="rest">N</span>
-          <span className="rest">D</span>
+          <div className="first-word">
+            <span className="initial">F</span>
+            <span className="rest">O</span>
+            <span className="rest">U</span>
+            <span className="rest">N</span>
+            <span className="rest">D</span>
+            <span className="rest">+</span>
+          </div>
+          <div className="second-word">
+            <span className="initial">G</span>
+            <span className="rest">R</span>
+            <span className="rest">O</span>
+            <span className="rest">U</span>
+            <span className="rest">N</span>
+            <span className="rest">D</span>
+          </div>
         </span>
       </h1>
       <nav>
